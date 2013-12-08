91dc8c2224db0f9ab7db3f544f1f10f7646710b4
Richard King <mail@richardskingdom.net>
2013-12-08 15:41:19 +0000

    HTTP client

---
+++ httpclient.js
@@ -0,0 +1,10 @@
+http = require('http');
+
+http.get(process.argv[2], function (response) {
+
+  response.setEncoding('utf8');
+  response.on('data', function (data) {
+    console.log(data);
+  });
+
+});
