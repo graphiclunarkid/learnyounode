d6df57929567b1b5ccb3ffc6e94df20a26ff16b0
Richard King <mail@richardskingdom.net>
2013-12-11 17:34:45 +0000

    Time server

---
+++ timeserver.js
@@ -0,0 +1,29 @@
+// Parameters: port
+// Output format YYYY-MM-DD hh:mm, zero-filled to two integers
+
+net = require('net');
+
+function pad(n) {
+	return n < 10 ? '0' + n : n;
+}
+
+function now() {
+	var date = new Date();
+	output = date.getFullYear();
+	output += '-';
+	output += pad(date.getMonth() + 1);
+	output += '-';
+	output += pad(date.getDate());
+	output += ' ';
+	output += pad(date.getHours());
+	output += ':';
+	output += pad(date.getMinutes());
+	return output;
+}
+
+var server = net.createServer(function (socket) {
+	socket.end(now() + '\n');
+});
+
+server.listen(Number(process.argv[2]));
+
